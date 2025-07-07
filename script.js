d2f15a0b1df1b453332394eedecdafb51b1cca03
valevurro <vale.v.00@outlook.it>
2025-07-07 20:54:02 +0200

    Add ticket pricing logic with user input for distance and age

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ per controllare che la vostra logica sui prezzi funzioni correttamente, provate 
 100km, 70 anni => prezzo corretto: €12.60
  */
 
+//chiede quanti km vuole percorrere l'utente
+const km = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
+
+// chiede l'età dell'utente
+const eta = parseInt(prompt("Quanti anni hai?"));
+
+// calcola il prezzo in base ai km senza sconto
+let prezzoBiglietto = km * 0.21;
+
+// applica lo sconto se minorenni 20% con valore 0.20 se over 65 sconto 40% con valore 0.40
+if (age < 18) {
+    discount = 20; // sconto del 20%
+
+} else if (age > 65) {
+    discount = 40; // sconto del 40%
+}
+
+// arrotonda il prezzo a due decimali
+prezzoBiglietto = prezzoBiglietto.toFixed(2);
+
+// mostra il prezzo finale all'utente
+const finalPrice = (ticketPrice * (1 - discount / 100));
+
